120be57c826d35efb9396e873b8bdb4bbbfaf825
Pierre Bausière <pbausiere@sierrawireless.com>
2018-01-16 17:37:53 +0100

    <feat> continuing services

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,69 +1,48 @@
-
 const service = require("../services/UserService")();
 
 
 
-exports.authenticate = function(req, res){
-	if(req.headers.authorization){
-		const b64 = req.headers.authorization.substring(6);
-		const buf = Buffer.from(b64, 'base64');
-		
-		const creds = buf.toString("utf-8").split(":");
-		console.log(creds);
-		const login = service.getJwt({login:creds[0], pwd:creds[1]});
-		res.json(login);
-	}
-	res.status(401);
+exports.authenticate = function(req, res) {
+  if (req.headers.authorization) {
+    const b64 = req.headers.authorization.substring(6);
+    const buf = Buffer.from(b64, 'base64');
+
+    const creds = buf.toString("utf-8").split(":");
+    console.log(creds);
+    const login = service.getJwt({
+      login: creds[0],
+      pwd: creds[1]
+    });
+    res.json(login);
+  }
+  res.status(401);
 }
 
-exports.getAllUsers = function(req, res){
-	console.log("Get all users");
-	User.find({}, function(err, user){
-		if(err){
-			res.send(err);
-		}
-		res.json(user);
-	});
+exports.getAllUsers = function(req, res) {
+
 };
 
-exports.createUser = function(req, res){
-	console.log("Create user");
-	const newUser = new User(req.body);
-	newUser.save(function(err, user){
-		if(err){
-			res.send(err);
-		}
-		res.json(user);
-	});
+exports.createUser = function(req, res) {
+
 };
 
-exports.getUser = function(req, res){
-	console.log("Get user");
-	//...findById({req.params.userId} ...
-	User.findOne({login : req.params.userId}, function(err, user){
-		if(err){
-			res.send(err);
-		}
-		res.json(user);
-	});
+exports.getUser = function(req, res) {
+  try {
+    const user = service.getUserByLogin(req.params.userId);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
-exports.updateUser = function(req, res){
-	console.log("Update user");
-	User.findOneAndUpdate({login:req.params.userId}, req.body, {new:true}, function(err, user){
-		if(err){
-			res.send(err);
-		}
-		res.json(user);
-	});
+exports.updateUser = function(req, res) {
+
 };
 
-exports.deleteUser = function(req, res){
-	console.log("Delete user");
-	User.remove({_id:req.params.userId}, function(err, user){
-		if(err){
-			res.send(err);
-		}
-		res.json({message:'User successfully deleted'});
-	});
+exports.deleteUser = function(req, res) {
+
 };
